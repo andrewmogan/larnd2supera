9a907e98bf93362cc35450c525fb2ba109bda8df
Andrew Mogan <andrew.mogan@colostate.edu>
2022-06-01 08:31:44 -0700

    Update setup.py
    setup.py previously contained references to edep-sim and
    DeepLearnPhysics. Replaced those instances with larndsim and this
    repository, respectively.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="edep2supera",
+    name="larnsim2supera",
     version="0.0.1",
     cmake_source_dir='src/',
     include_package_data=True,
@@ -15,17 +15,19 @@ setup(
         #'-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON',
         '-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9',
     ],
-    author=['Kazuhiro Terao'],
-    author_email='kterao@slac.stanford.edu',
-    description='Supera interface for EDepSim input data files',
-    license='MIT',
-    keywords='supera edep-sim larcv larcv3 neutrinos deep learning lartpc_mlreco3d',
+    author=['Andrew Mogan'],
+    author_email='andrew.mogan@colostate.edu',
+    description='Supera interface for larndsim input data files',
+    #license='MIT',
+    keywords='supera larndsim larcv larcv3 neutrinos deep learning lartpc_mlreco3d',
     project_urls={
-        'Source Code': 'https://github.com/DeepLearnPhysics/edep2supera'
+        #'Source Code': 'https://github.com/DeepLearnPhysics/larnsim2supera'
+        'Source Code': 'https://github.com/andrewmogan/larndsim2supera'
     },
-    url='https://github.com/DeepLearnPhysics/edep2supera',
+    #url='https://github.com/DeepLearnPhysics/edep2supera',
+    url='https://github.com/andrewmogan/larndsim2supera',
     scripts=[],
-    packages=['edep2supera'],
+    packages=['larnsim2supera'],
     package_dir={'': 'python'},
     install_requires=[
         'numpy',
